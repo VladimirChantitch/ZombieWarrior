cb0944104a582b42f2c03e128363e67479a4f210
Vladimir Chantitch <chantitch.vladimir@gmail.com>
2023-04-12 10:11:10 +0200

    Added SignInElement

---
+++ Assets/01_Script/UI/SignInElement.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UI
+{
+    public class SignInElement : VisualElement
+    {
+        public new class UxmlFactory : UxmlFactory<SignInElement, SignInElement.UxmlTraits> { }
+
+        public event Action<string> onNewPlayerSignIn;
+        public event Action onCancel;
+
+        TextField tf_name = null;
+        Button btn_validate = null;
+        Button btn_cancel = null;
+
+        public void Init()
+        {
+            BindButtons();
+        }
+
+        private void BindButtons()
+        {
+            btn_cancel.clicked += () => onCancel.Invoke();
+            btn_validate.clicked += () => onNewPlayerSignIn.Invoke(tf_name.text);
+        }
+    }
+}
+
